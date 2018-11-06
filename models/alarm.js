8009850b1f00b77abacee6d257c2af33dae781a3
dorian <ciy405x@kaist.ac.kr>
2018-11-06 07:09:20 +0000

    alarm.js

--- models/alarm.js
+++ models/alarm.js
@@ -15,12 +15,3 @@ const alarmSchema = Schema({
 	
 });
 
-// Make a connection
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', () => {console.log("Alarm.js: Connected to mongodb server.")});
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/cs4cs', { useMongoClient: true });
-
-module.exports = mongoose.model('alarm', alarmSchema);
