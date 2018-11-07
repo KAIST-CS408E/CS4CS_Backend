c3b628396d1d6a20d839b730c81946a321aae4b8
silvershine157 <silvershine157@kaist.ac.kr>
2018-11-07 09:19:26 +0000

    check reporter is kaistian

--- api/report.js
+++ api/report.js
@@ -6,18 +6,30 @@ exports.makeNewAlarm = (lat, lng, rad, title, cat_str, desc, reporter) =>
 
     new Promise((resolve,reject) => 
     {
-        const newAlarm = new Alarm({
-	        lat: lat,
-            lng: lng,
-	        rad: rad,
-	        title: title,
-	        cat_str: cat_str,
-	        desc: desc,
-            reporter: reporter,
-	        created_at: new Date()
+        User.find({email: reporter})
+        .then(users => {
+            if(users.length == 0){
+                reject({status: 401, message: 'User is not registered'});
+            }
+            else if(!users[0].kaistian){
+                reject({status: 401, message: 'User is not Kaistian'});
+            }
+            else{
+                 const newAlarm = new Alarm({
+	                lat: lat,
+                    lng: lng,
+	                rad: rad,
+	                title: title,
+	                cat_str: cat_str,
+	                desc: desc,
+                    reporter: reporter,
+	                created_at: new Date()
+                });
+                return newAlarm.save()
+                .then(() => {
+	                resolve({status: 201, message: 'Alarm is reported to server (title:'+title+')'});
+                });
+            }
         });
-        return newAlarm.save()
-        .then(() => {
-	        resolve({status: 201, message: 'Alarm is reported to server (title:'+title+')'});
-        });
-    });
+    };
+
