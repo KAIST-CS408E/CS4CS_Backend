e24c508536dd5d9d6b144015dae49fe5a7be3c16
Dorian Gray <00076289@kaist.ac.kr>
2018-11-15 12:59:58 +0000

    fcm server

--- test.js
+++ test.js
@@ -1,1 +1,31 @@
 var admin = require('firebase-admin');
+var registrationToken = 'fFdYhqQ1CxE:APA91bEAdS-Pe_py6ZSDumLfeQQ1GahX3tuYc_u-ysMf3glQwaCFOLUjeFTc3BZrRU2Tlxij4e2tAOc2ZTRIFMVZgJV3y4KH_8JIp__oHXONuAlytZYaIGhjKhovUUKrrgQ62iDSPiJx';
+var serviceAccount = require('./cs4cs-d1f39-firebase-adminsdk-vnkqd-00261e2dfc.json');
+
+var defaultApp = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+});
+
+var topic = 'alarm';
+var dryRun = true;
+
+var message = {
+    notification: {
+        title: "Test",
+        body: "test"
+    },
+    data: {
+        lat: '1',
+        lng: '1'
+    },
+    token: registrationToken
+};
+
+admin.messaging().send(message)
+    .then((response) => {
+        console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+        console.log('Error sending message:', error);
+    });        
+        
