99812f7f64bf8f6990f51ee6cefecbdc1d300be1
Dorian Gray <00076289@kaist.ac.kr>
2018-11-16 11:02:29 +0000

    for merge

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var admin = require('firebase-admin');
 var registrationToken = 'fFdYhqQ1CxE:APA91bEAdS-Pe_py6ZSDumLfeQQ1GahX3tuYc_u-ysMf3glQwaCFOLUjeFTc3BZrRU2Tlxij4e2tAOc2ZTRIFMVZgJV3y4KH_8JIp__oHXONuAlytZYaIGhjKhovUUKrrgQ62iDSPiJx';
+var inyoungsToken = 'eaF84Mtd5WA:APA91bGUlzxBjAQQAydyJX8kFbbT_S3XipPq763RV02mGbxSbZ-fRULVoaj4mljMoj1FAvE6I5wDk6jed3wJGrJivjCsbypndD7yYRYi55iPqLfRmHRCOvZEL6446v20TOT4lpZgoXdU';
 var serviceAccount = require('./cs4cs-d1f39-firebase-adminsdk-vnkqd-00261e2dfc.json');
 
 var defaultApp = admin.initializeApp({
@@ -15,7 +16,8 @@ var message = {
         title: 'zebal',
         body: 'please'
     },
-    token: registrationToken
+    topic: topic
+//    token: registrationToken
 };
 
 admin.messaging().send(message)
@@ -25,4 +27,5 @@ admin.messaging().send(message)
     .catch((error) => {
         console.log('Error sending message:', error);
     });        
-        
+
+console.log("test executing");
