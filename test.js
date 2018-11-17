5f455783e9e0e6204b2743d552b3b1fa671da904
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2018-11-18 01:17:41 +0900

    fix conflict in test.js

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var admin = require('firebase-admin');
-<<<<<<< HEAD
-var registrationToken = 'fFdYhqQ1CxE:APA91bEAdS-Pe_py6ZSDumLfeQQ1GahX3tuYc_u-ysMf3glQwaCFOLUjeFTc3BZrRU2Tlxij4e2tAOc2ZTRIFMVZgJV3y4KH_8JIp__oHXONuAlytZYaIGhjKhovUUKrrgQ62iDSPiJx';
-var inyoungsToken = 'eaF84Mtd5WA:APA91bGUlzxBjAQQAydyJX8kFbbT_S3XipPq763RV02mGbxSbZ-fRULVoaj4mljMoj1FAvE6I5wDk6jed3wJGrJivjCsbypndD7yYRYi55iPqLfRmHRCOvZEL6446v20TOT4lpZgoXdU';
-=======
-var registrationToken = 'eTA1DM-NUCY:APA91bEjGXNNyCrlnVYiDTwngyh115LHCVSxaE2o9SfcVL0IgjIdIl2DRFGhRwV8npSusx-pbz_G9v3EjOhYRfe5bcNRElLqjhQfilKKPru_3CWRFyEx7E6gffH7KpbukpU9fVGgep1G';
->>>>>>> 966b30e9b3e31a571e07c1da0889315b7272b8d2
+
+var taeToken = 'fFdYhqQ1CxE:APA91bEAdS-Pe_py6ZSDumLfeQQ1GahX3tuYc_u-ysMf3glQwaCFOLUjeFTc3BZrRU2Tlxij4e2tAOc2ZTRIFMVZgJV3y4KH_8JIp__oHXONuAlytZYaIGhjKhovUUKrrgQ62iDSPiJx';
+var youngToken1 = 'eaF84Mtd5WA:APA91bGUlzxBjAQQAydyJX8kFbbT_S3XipPq763RV02mGbxSbZ-fRULVoaj4mljMoj1FAvE6I5wDk6jed3wJGrJivjCsbypndD7yYRYi55iPqLfRmHRCOvZEL6446v20TOT4lpZgoXdU';
+var shinToken = 'eTA1DM-NUCY:APA91bEjGXNNyCrlnVYiDTwngyh115LHCVSxaE2o9SfcVL0IgjIdIl2DRFGhRwV8npSusx-pbz_G9v3EjOhYRfe5bcNRElLqjhQfilKKPru_3CWRFyEx7E6gffH7KpbukpU9fVGgep1G';
+var youngToken2 = 'c-j9hUE0-JU:APA91bH0N4xFjGNb4FIOYNOEIWCQkn-eAx9Cy921ST8TkeJ5C8iJR5MDbpVDRNctSrZIzVhu2g5kW1UJIAbVinfhc-9OXZiwNpFeQNVTX9xMQ8WfkOL3VB096qXl4F8V2hLjV9ZhBMZ-';
+
 var serviceAccount = require('./cs4cs-d1f39-firebase-adminsdk-vnkqd-00261e2dfc.json');
 
 var defaultApp = admin.initializeApp({
