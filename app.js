f01c3c50f48e3005dc0d180e66e54d44b7e24345
Dorian Gray <00076289@kaist.ac.kr>
2018-11-19 11:04:14 +0000

    make periodic function

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const router = express.Router();
-const port = process.env.port || 8001;
+const port = process.env.port || 8002;
 const route = require('./routes/index');
 const mongoose = require('mongoose');
-
+const Alarm = require('./models/alarm');
+const performnace = require('perf_hooks').performance;
 // Mount middlewares
 app.use(bodyParser.json());
 app.use(logger('dev'));
@@ -25,3 +26,21 @@ mongoose.connect('mongodb://localhost:27017/cs4cs', { useMongoClient: true });
 // Start listening
 app.listen(port);
 console.log(`App runs on ${port}.`);
+
+//Send all alarms for infinite time
+function sendAlarm(){
+    console.log("sendAlarm");
+    
+    console.time('check');
+    Alarm.find(function(err, alarms) {
+        if (err) throw err;
+        
+        for (var i = 0; i < alarms.length; i++)
+            console.log(alarms[i].rad);
+
+        console.timeEnd('check');
+    });
+    
+}
+
+setInterval(sendAlarm, 10000);
